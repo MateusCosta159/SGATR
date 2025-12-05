5e55c75f4a79f2248694237345610e57a0d9b75b
Mateus Costa da Silva <mcostasilva2005@gmail.com>
2025-12-04 22:20:54 -0300

    Configurando conexão com banco de dados, padrão DAO

--- src/java/Aulas/xml/Teste.java
+++
@@ -1,9 +0,0 @@
-package Aulas.xml;
-
-import logtrack.ExceptionLogTrack;
-
-public class Teste {
-    public static void main(String[] args) {
-        ExceptionLogTrack.getInstance().addLog(new Exception("teste"));
-    }
-}
